f7edadf755d08463ffa79ebc2204789824d64646
Neil Woodhouse <clonesupreme@gmail.com>
2022-04-25 21:23:48 +0100

    Added comments to Settings

--- WindowsFormsApp1/Settings.cs
+++ WindowsFormsApp1/Settings.cs
@@ -12,17 +12,20 @@ namespace LoRaChat
 {
     public partial class Settings : Form
     {
+        // Initialises the settings window
         public Settings()
         {
             InitializeComponent();
         }
 
+        // Saves username changes to non-volatile settings file
         private void username_box_TextChanged(object sender, EventArgs e)
         {
             Properties.Settings.Default.username = username_box.Text;
             Properties.Settings.Default.Save();
         }
 
+        // Saves endpoint WebSocket address changes to non-volatile settings file
         private void node_address_TextChanged(object sender, EventArgs e)
         {
             Properties.Settings.Default.ws_path = node_address.Text;
